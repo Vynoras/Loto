74f446917ba83ff76c9fb97b0e8fdb971b756e36
Vynoras <ernuz61@gmail.com>
2025-04-13 21:15:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
-      <h1>Süper Loto Analiz</h1>
-      <p>Yapay zekâ destekli loto sistemin yayında!</p>
+      <h1>Süper Loto Kupon Öneri Sistemi</h1>
+      <p>Yapay zekâ destekli loto tahminleri burada!</p>
+      <p>Sonuçları analiz ediyor, en olası kombinasyonları öneriyoruz.</p>
     </div>
   );
 }
