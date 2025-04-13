98718d6a6faae9094308ae95beb462ece6dd6b6a
Vynoras <ernuz61@gmail.com>
2025-04-13 20:33:12 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Süper Loto Analiz</h1>
+      <p>Yapay zekâ destekli loto sistemin yayında!</p>
+    </div>
+  );
+}
